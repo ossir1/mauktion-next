2524b115bcd6c2fce79bf48cba4598d321d17b78
ossir1 <ossirikkila@gmail.com>
2025-04-07 01:33:14 +0300

    Create layout.tsx

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+// components/Layout.tsx
+import Header from './Header'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  )
+}
