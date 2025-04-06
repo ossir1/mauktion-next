5348c5eeac86267094c7a5b966c9d5e1721e0d65
ossir1 <ossirikkila@gmail.com>
2025-04-06 15:36:37 +0300

    Create products.ts

---
+++ data/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Vintage Watch",
+    price: "120 €",
+    buyNow: true,
+    auction: false
+  },
+  {
+    id: 2,
+    name: "Retro Chair",
+    price: "50 €",
+    buyNow: false,
+    auction: true
+  },
+  {
+    id: 3,
+    name: "Rare Vinyl Record",
+    price: "85 €",
+    buyNow: true,
+    auction: true
+  }
+];
