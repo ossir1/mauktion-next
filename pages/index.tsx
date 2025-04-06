fb8ef77c1abe9254c3efa9224490ae94783eb588
ossir1 <ossirikkila@gmail.com>
2025-04-06 21:39:17 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import ProductCard from '../components/ProductCard'
 import { products } from '../data/products'
-import { useEffect, useState } from 'react'
+import Header from '../components/Header'
 
 export default function Home() {
   const [allProducts, setAllProducts] = useState(products)
@@ -10,7 +11,7 @@ export default function Home() {
     const added = localStorage.getItem('mauktion-added-product')
     if (added) {
       const newProduct = JSON.parse(added)
-      setAllProducts((prev) => [...prev, newProduct])
+      setAllProducts((prev) => [newProduct, ...prev])
       localStorage.removeItem('mauktion-added-product')
     }
   }, [])
@@ -20,20 +21,11 @@ export default function Home() {
       <Head>
         <title>Mauktion Demo</title>
       </Head>
-      <main className="p-6">
-        <div className="flex justify-between items-center mb-4">
-<a href="/my-products" className="text-blue-600 underline mb-4 block">
-  → Siirry omiin tuotteisiin
-</a>
 
-          <h1 className="text-3xl font-bold">Welcome to Mauktion</h1>
-          <a
-            href="/add"
-            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition"
-          >
-            Lisää tuote
-          </a>
-        </div>
+      <Header />
+
+      <main className="p-6 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Tervetuloa Mauktioniin</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {allProducts.map((product) => (
             <ProductCard
