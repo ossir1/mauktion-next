6f67e1bab6a4a352ad2c7fd71b5b2f0a30df2d93
ossir1 <ossirikkila@gmail.com>
2025-04-07 04:26:32 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
+      <Header />
       <Component {...pageProps} />
-    </Layout>
+    </>
   )
 }
